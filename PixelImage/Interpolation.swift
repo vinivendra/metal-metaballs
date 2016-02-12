4516e09b583ad2adca6b8af970e75505a2a3604f
vinivendra <vinivendra@gmail.com>
2016-02-12 00:40:30 -0500

    Encapsulated a few interpolation functions.

---
+++ PixelImage/Interpolation.swift
@@ -0,0 +1,17 @@
+
+func interpolateCubeEaseIn<T: Numeric>(linear: T) -> T {
+    return linear ^ 3
+}
+
+func interpolateSquareEaseIn<T: Numeric>(linear: T) -> T {
+    return linear ^ 2
+}
+
+func interpolateSquareEaseOut<T: Numeric>(linear: T) -> T {
+    return 1 - (1 - linear) * (1 - linear)
+}
+
+func interpolateSmooth<T: Numeric>(linear: T) -> T {
+    return ((linear) * (linear) * (linear) * ((linear) * ((linear) * 6 - 15) + 10))
+}
+
