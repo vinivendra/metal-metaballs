9392fc76572a691a09543137f8d8cf374df86cee
vinivendra <vinivendra@gmail.com>
2016-02-25 15:25:27 -0500

    Improved initial edge animation.

--- PixelImage/ViewController.swift
+++ PixelImage/ViewController.swift
@@ -56,6 +56,10 @@ class ViewController: UIViewController, MetaballDataSource {
 
         addEdge(0, 2)
 
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2 * Int64(NSEC_PER_SEC)), dispatch_get_main_queue()) {
+            self.removeEdge(0, 2)
+        }
+
         renderer.state = .Running
     }
 
